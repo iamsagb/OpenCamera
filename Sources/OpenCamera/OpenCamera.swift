ae29f436fca4d11f9ca104158cad683297e379f9
Sagar Bhosale <88615636+iamsagb@users.noreply.github.com>
2024-08-21 15:13:57 +0530

    initial commit

---
+++ Sources/OpenCamera/OpenCamera.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import Foundation
+
+public struct OpenCamera {
+    public init() {}
+    
+    public func createCamera() {
+        print("Smile!!")
+    }
+}
